acec50a427151a562b7e188c7381a0e9e64ab7c1
Hikari0901 <rexj321@gmail.com>
2024-07-15 23:47:17 -0500

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lgyy76h!+xuxgbmt&jnkeytrdq5rst0-jyl=i2)&p5=lx%yy-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://mysite-upc1.onrender.com']
+ALLOWED_HOSTS = ['34.82.93.227','mysite-upc1.onrender.com']
 
 
 # Application definition
