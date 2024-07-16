95f155ae351cb05c8048376f19e01c75e7a975d4
Hikari0901 <rexj321@gmail.com>
2024-07-15 23:03:53 -0500

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for pagina project.
 
@@ -76,10 +77,11 @@ WSGI_APPLICATION = 'pagina.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
